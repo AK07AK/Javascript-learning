d9a7d4436ba213ede3ecded2b208be1ebaa5e223
arun <aruntechgeek15@gmail.com>
2024-08-01 12:41:41 +0530

    All datatypes

--- datatypes.js
+++ datatypes.js
@@ -5,3 +5,15 @@ boolean
 undefined
 null
 */
+
+let name = 'arun'
+let age = 30;
+let hasRight = true;
+let phoneNumber=undefined;
+let salary = null;
+
+console.log(name);
+console.log(age);
+console.log(hasRight);
+console.log(phoneNumber);
+console.log(salary);
