9c3777a78330306642a5ad366765b77e3711093e
arun <aruntechgeek15@gmail.com>
2024-08-02 21:31:22 +0530

    Arrays file

---
+++ arrays.js
@@ -0,0 +1,7 @@
+let selectedColors = [];
+selectedColors[0]="arun";
+selectedColors[1]=10;
+selectedColors[3]=null;
+selectedColors[2]=true;
+console.log(selectedColors);
+console.log(typeof(selectedColors));
